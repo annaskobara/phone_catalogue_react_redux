44937f78c0333a9d61bf2a9c3d0aeb01530c6808
Anna Skobara <annaskobara14@gmail.com>
2026-06-26 14:26:32 +0300

    fix: use relative paths for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/phone_catalogue_react_redux/',
+
+  base: './',
   plugins: [react()],
   css: {
     preprocessorOptions: {
