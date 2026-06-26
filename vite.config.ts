4c5426b7a2470526b22c4223ec64ba288773500c
Anna Skobara <annaskobara14@gmail.com>
2026-06-26 14:09:12 +0300

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/phone_catalogue_react_redux/',
   plugins: [react()],
   css: {
     preprocessorOptions: {
