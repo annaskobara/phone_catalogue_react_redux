d143fef31342f32e9ffb0bdc65510f794789558f
Anna Skobara <annaskobara14@gmail.com>
2026-07-24 15:24:39 +0300

    fix: update vite base path and homepage for github pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-
-  base: './',
+  base: '/phone_catalogue_react_redux/',
   plugins: [react()],
   css: {
     preprocessorOptions: {
