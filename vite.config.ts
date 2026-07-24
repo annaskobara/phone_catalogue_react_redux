570d8ae01161f760882c45a7c558f7cacba69923
Anna Skobara <annaskobara14@gmail.com>
2026-07-24 15:36:18 +0300

    fix: update script src in index.html to relative path for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: './',
   plugins: [react()],
+  build: {
+    sourcemap: false,
+  },
   css: {
     preprocessorOptions: {
       scss: {
